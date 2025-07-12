a74bbcd1953a8c171f4f05bddd65de0b507d4bf0
SG Prakash <sgp.prakas@gmail.com>
2025-07-12 14:16:43 +0530

    check pr events

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ console.log("Check once more - 2");
 console.log("Check once more - 3");
 console.log("Check once more - 4");
 console.log("Finally, check once more - 5");
+
+console.log("Can we check pr events now?");
